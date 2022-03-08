8596a37b981d03e5bd8b2cd7134b65f266aa060c
Kashish Jaiswal <kashishjais23@gmail.com>
2022-03-08 18:26:23 +0530

    .

--- hp_app/app.py
+++ hp_app/app.py
@@ -2,20 +2,22 @@ from flask import Flask, render_template,request
 import  numpy  as np
 from joblib import load
 import os
+import xgboost as xgb
 
 
 
 def load_hp_model():
-  filepath='Machine-Learning/hp_app/house_price_model.xgb'
-  return load(filepath)
+  filepath='house_pricing_model.xgb'
+  saved_model=xgb.Booster()
+  saved_model.load_model(filepath)
+  return saved_model
 
 def predict(Beds=0,Baths=0,SquareFeet=0):
   userinp=np.array([[Beds,Baths,SquareFeet]])
-  model_dict=load_hp_model()
-
-  x=model_dict.get('scaler').transform(userinp)
-  p=model_dict.get('classifier').predict(x)
-  return p[0]
+  model=load_hp_model()
+  out=model.predict(xgb.DMatrix(userinp))
+  return out[0]
+  
 
 
 app = Flask(__name__)
